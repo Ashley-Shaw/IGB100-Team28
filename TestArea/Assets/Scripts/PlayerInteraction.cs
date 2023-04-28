a6fbd7ef35a68f3dc78f9711bf1b4473c0e84146
Sacobite <130025400+Sacobite@users.noreply.github.com>
2023-04-28 21:42:33 +1000

    Code changes
    Additions to items, escape, text, etc

--- TestArea/Assets/Scripts/PlayerInteraction.cs
+++ TestArea/Assets/Scripts/PlayerInteraction.cs
@@ -9,49 +9,78 @@ public class PlayerInteraction : MonoBehaviour
 {
 
 
-    float raycastDistance = 3; 
+    float raycastDistance = 3;
 
-    public TMP_Text interactText; 
+    public TMP_Text interactText;
 
     public TMP_Text Score;
+
+    public TMP_Text GameOverText;
 
     public int Counter = 0;
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        RaycastHit hit; 
+        RaycastHit hit;
 
         Score.text = "Item Count:" + Counter + "/4";
 
 
-        if (Physics.Raycast(ray, out hit, raycastDistance)) 
+        if (Physics.Raycast(ray, out hit, raycastDistance))
         {
-            
+
             if (hit.collider.CompareTag("Item"))
             {
-                interactText.text = "Press [E] to read the note."; 
-                
+                interactText.text = "Press E to pick up";
+
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    Debug.Log("[E] Was Pressed while looking at a note, lets open the note.");
                     Counter += 1;
                     Destroy(hit.transform.gameObject);
 
 
                 }
-          
-             
+
+
             }
 
-            else //If nothing at all with an above tag was hit with the Raycast within the specified distance then run this
+            else if (hit.collider.CompareTag("Door"))
             {
-                if (interactText.text != "")//If the interactText is not already set as nothing then set it to nothing - this is to help optimise and save from constantly spamming this request
+                interactText.text = "Press E to escape";
+
+                if (Input.GetKeyDown(KeyCode.E))
                 {
-                    interactText.text = ""; //Removing the text as nothing was detected by the raycast
+                    GameOverText.text = "You Escaped!";
+
                 }
+
+
+            }
+
+            else if (hit.collider.CompareTag("Bars"))
+            {
+                interactText.text = "Press E to open the door";
+                if (Counter == 4)
+
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        Destroy(hit.transform.gameObject);
+
+                    }
+            }
+
+        }
+
+        else
+        {
+            if (interactText.text != "")
+            {
+                interactText.text = "";
             }
         }
+
+
     }
 }
