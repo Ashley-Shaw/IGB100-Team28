a61e8debf5b313982d6710c331f54ba3f5ae9332
PmoL <kzhou1112@outlook.com>
2023-05-15 20:05:39 +1000

    terrain and minor update

--- TestArea/Assets/Allart/enviormentAssets/RotateY.cs
+++ TestArea/Assets/Allart/enviormentAssets/RotateY.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class RotateY : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 10f; // The speed of rotation.
+    
 
     void Update()
     {
-        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        transform.Rotate(0.0f,0.1f, 0.0f, Space.Self);
     }
 }
